6ebf61860bf856c674836a6c890a6228861628b9
Hachem Zarrad <hachemzarrad@gmail.com>
2021-07-24 17:14:49 +0100

    News Model

--- app/src/main/java/com/example/latestnews/Models/News.kt
+++ app/src/main/java/com/example/latestnews/Models/News.kt
@@ -1,4 +1,17 @@
 package com.example.latestnews.Models
 
 class News {
+    var id: String? = null
+    var title: String? = null
+    var external_link: String? = null
+    var thumbnail: String? = null
+    var created_at: String? = null
+    var updated_at: String? = null
+    var state_id: String? = null
+    var place_id: String? = null
+    var category_id: String? = null
+    var expire: String? = null
+    var html: String? = null
+    var publish_date: String? = null
+    var place: String? = null
 }
