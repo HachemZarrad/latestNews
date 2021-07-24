57c9e40bd9271a94593681f953bc349559c7a8f7
Hachem Zarrad <hachemzarrad@gmail.com>
2021-07-24 17:59:36 +0100

    Setting url

--- app/src/main/java/com/example/latestnews/Common/Common.kt
+++ app/src/main/java/com/example/latestnews/Common/Common.kt
@@ -1,4 +1,11 @@
 package com.example.latestnews.Common
 
+import com.example.latestnews.Interfaces.RetrofitService
+import com.example.latestnews.Retrofit.RetrofitClient
+
 object Common {
+    private val BaseUrl = "https://mobilapp.kozbeszerzes.hu/api/content/"
+
+    val retrofitService: RetrofitService
+    get() = RetrofitClient.getClient(BaseUrl).create(RetrofitService::class.java)
 }
