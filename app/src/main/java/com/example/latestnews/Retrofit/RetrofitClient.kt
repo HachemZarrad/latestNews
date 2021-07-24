e89db17dd300e3d1e6b934e2e71f1b64c3a94669
Hachem Zarrad <hachemzarrad@gmail.com>
2021-07-24 17:36:51 +0100

    Importing Dependencies

--- app/src/main/java/com/example/latestnews/Retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/latestnews/Retrofit/RetrofitClient.kt
@@ -1,5 +1,8 @@
 package com.example.latestnews.Retrofit
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
     private var retrofit: Retrofit? = null
 
