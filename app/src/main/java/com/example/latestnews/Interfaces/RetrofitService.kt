0356d85cf80bda3e03075472d9761e06d7b712a0
Hachem Zarrad <hachemzarrad@gmail.com>
2021-07-24 17:49:11 +0100

    Inmplementing Retrofit interface

--- app/src/main/java/com/example/latestnews/Interfaces/RetrofitService.kt
+++ app/src/main/java/com/example/latestnews/Interfaces/RetrofitService.kt
@@ -1,4 +1,11 @@
 package com.example.latestnews.Interfaces
 
+import com.example.latestnews.Models.Article
+import retrofit2.http.GET
+import retrofit2.Call
+
+
 interface RetrofitService {
+    @GET("articlesByCategory?id=13")
+    fun getArticlesList() : Call<MutableList<Article>>
 }
