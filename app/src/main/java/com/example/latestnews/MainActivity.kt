d866786ee711365b945c84f9924997369cb2e3c3
Hachem Zarrad <hachemzarrad@gmail.com>
2021-07-25 02:16:53 +0100

    Data Fetched And Displayed Successfully

--- app/src/main/java/com/example/latestnews/MainActivity.kt
+++ app/src/main/java/com/example/latestnews/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.latestnews
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.latestnews.Adapters.ArticleAdapter
+import com.example.latestnews.Common.Common
+import com.example.latestnews.Interfaces.RetrofitService
+import com.example.latestnews.Models.Article
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mService : RetrofitService
+    lateinit var layoutManager: LinearLayoutManager
+    lateinit var adapter : ArticleAdapter
+    lateinit var dialog: AlertDialog
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mService = Common.retrofitService
+
+        recyclerArticlesList.setHasFixedSize(true)
+        layoutManager = LinearLayoutManager(this)
+        recyclerArticlesList.layoutManager = layoutManager
+
+        dialog = SpotsDialog.Builder().setCancelable(false).setContext(this).build()
+
+        getArticlesList()
+    }
+
+    private fun getArticlesList() {
+        dialog.show()
+
+        mService.getArticlesList().enqueue(object : Callback<MutableList<Article>> {
+            override fun onFailure(call: Call<MutableList<Article>>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call<MutableList<Article>>, response: Response<MutableList<Article>>) {
+                adapter = ArticleAdapter(baseContext, response.body() as MutableList<Article>)
+                adapter.notifyDataSetChanged()
+                recyclerArticlesList.adapter = adapter
+
+                dialog.dismiss()
+            }
+
+        })
     }
 }
