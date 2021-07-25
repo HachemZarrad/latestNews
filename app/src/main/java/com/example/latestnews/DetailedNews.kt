d23581a6ddb0320d3faac6bca54118943cd18c15
Hachem Zarrad <hachemzarrad@gmail.com>
2021-07-25 08:13:05 +0100

    Implementing DetailesNews Activity

--- app/src/main/java/com/example/latestnews/DetailedNews.kt
+++ app/src/main/java/com/example/latestnews/DetailedNews.kt
@@ -2,10 +2,26 @@ package com.example.latestnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_detailed_news.*
 
 class DetailedNews : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detailed_news)
+
+
+        val bundle: Bundle?= intent.extras
+        val imageUrl = bundle!!.getString("image")
+        val publishDate = bundle!!.getString("date")
+        val articleTitle = bundle!!.getString("title")
+        val articleDescription = bundle!!.getString("description")
+
+
+        Picasso.get().load(imageUrl).into(image)
+        date.text = publishDate
+        theTitle.text = articleTitle
+        description.text = articleDescription
+
     }
 }
